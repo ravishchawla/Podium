fa41781083d4196f36ba776e65338811660aa44e
emilywall <emilywall@gatech.edu>
2016-03-21 21:30:08 -0400

    changed color scheme to use shades of green and red rather than opacity

--- js/resources.js
+++ js/resources.js
@@ -3,11 +3,49 @@ const attributeStates = {"HIGH" : 1, "LOW" : 2, "UNUSED" : 3};
 /*Colors*/
 
 getPositiveMoveWithOpacity = function(opacity) { 
-	return 'rgba(88, 218, 91, ' + opacity + ')';
+	if (opacity > 0.9)
+		return 'rgb(18, 84, 19)';
+	else if (opacity > 0.8)
+		return 'rgb(22, 105, 24)';
+	else if (opacity > 0.7)
+		return 'rgb(27, 126, 28)';
+	else if (opacity > 0.6)
+		return 'rgb(31, 147, 33)';
+	else if (opacity > 0.5)
+		return 'rgb(36, 168, 38)';
+	else if (opacity > 0.4)
+		return 'rgb(40, 189, 43)';
+	else if (opacity > 0.3)
+		return 'rgb(45, 210, 47)';
+	else if (opacity > 0.2)
+		return 'rgb(66, 215, 68)';
+	else if (opacity > 0.1)
+		return 'rgb(87, 219, 89)';
+	
+	return 'rgb(108, 224, 110)';
 }
 
 getNegativeMoveWithOpacity = function(opacity) {
-	return 'rgba(218, 91, 88, ' + opacity + ')';
+	if (opacity > 0.9)
+		return 'rgb(84, 19, 18)';
+	else if (opacity > 0.8)
+		return 'rgb(105, 24, 22)';
+	else if (opacity > 0.7)
+		return 'rgb(126, 28, 27)';
+	else if (opacity > 0.6)
+		return 'rgb(147, 33, 31)';
+	else if (opacity > 0.5)
+		return 'rgb(168, 38, 36)';
+	else if (opacity > 0.4)
+		return 'rgb(189, 43, 40)';
+	else if (opacity > 0.3)
+		return 'rgb(210, 47, 45)';
+	else if (opacity > 0.2)
+		return 'rgb(215, 68, 66)';
+	else if (opacity > 0.1)
+		return 'rgb(219, 89, 87)';
+	
+	return 'rgb(224, 110, 108)';
 }
 
 const COLORS = {
@@ -15,8 +53,8 @@ const COLORS = {
 	"EVEN_COLUMN" : "#A68F58",
 	"ODD_COLUMN_EXPECT" : "#d399e8",
 	"EVEN_COLUMN_EXPECT" : "#969Deb",
-	"POSITIVE_MOVE" : "#DA5B58",
-	"NEGATIVE_MOVE" : "#58DA5B",
+	"POSITIVE_MOVE" : "#58DA5B",
+	"NEGATIVE_MOVE" : "#DA5B58",
 	"POSITIVE_MOVE_GRADIENT" : getPositiveMoveWithOpacity,
 	"NEGATIVE_MOVE_GRADIENT" : getNegativeMoveWithOpacity,
 	"MINIMAP_ROW" : "rgb(255, 255, 255)",
